167568d798880fd79b6515d4ee855f82e04615b1
faizala-synopsys <127382244+faizala-synopsys@users.noreply.github.com>
2023-03-13 17:22:47 +0530

    Add files via upload

---
+++ src/footer.php
@@ -0,0 +1,12 @@
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moein
+ * Date: 12/12/18
+ * Time: 1:43 PM
+ */
+?>
+<link rel="stylesheet" href="../static/css/bootstrap.min.css">
+<link rel="stylesheet" href="../static/css/font-awesome.min.css">
